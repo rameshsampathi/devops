83e437cccfb08406bff4172bab05c02cfd8588f9
rameshsampathi <60913896+rameshsampathi@users.noreply.github.com>
2021-06-04 21:28:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ terraform {
   backend "s3" {
     bucket ="sampathiterraformbucket"
     key = "terraform.tfstate"
+    dynamodb_table = "terraform-state-lock-dynamo"  
     region = "us-east-1"
   }
   
